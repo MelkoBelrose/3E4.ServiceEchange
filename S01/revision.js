d376dcf23156a66bdf7fe89b73ea5df6794bfa1e
Mathieu Soucy <melkobelrose@gmail.com>
2021-08-25 11:37:11 -0400

    fin cours 2

--- S01/revision.js
+++ S01/revision.js
@@ -17,4 +17,46 @@ function displayUser(name, age) {
 
 displayUser('Rosalie',12);
 
+//Collection, Tableau, Liste
+
+const fruits =['Kiwi','Banane','Fraise','Tomate','Mangue'];
+
+//Ecrit le tableau telle qu'elle
+console.log(fruits);
+
+
+//Facon des pourris
+for(let fruit of fruits){
+    console.log(fruit);
+}
+
+
+//Facons des vrai hommes, avec landa, fonction anonyme
+fruits.forEach(f => console.log(f));
+
+
+const sum =  (a, b) => a + b;
+const result = sum(2,5);
+console.log(result);
+
+const someFruits = fruits.filter(f => f.length < 5)
+console.log(someFruits);
+
+const numbers = [10,20,30,40];
+const MULTIPLIER = 3;
+const products = numbers.map(n => n * MULTIPLIER).filter(n => n > 75).map(n => n + 9);
+console.log(products);
+console.log(numbers);
+
+numbers.push(50);
+console.log(numbers);
+
+const avenger = {
+    alterEgo:'beter',
+    hero:'spodermon',
+    movies:[{title:'a'},{title:'g'},{title:'b'}]
+}
+
+console.log(avenger.alterEgo);
+console.log(avenger.movies.forEach(m => console.log(m.title)));
 
---
+++ S01/server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((request,response) => {
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain')
+    response.end('YO LES GENS.');
+    console.log(request);
+});
+
+server.listen(1337, '127.0.0.1', () =>{
+    console.log('The serveur is doing the travaille');
+})
