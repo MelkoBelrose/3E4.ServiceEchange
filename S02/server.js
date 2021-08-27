7aeea64e15c5a349dc8748dacb30dc1112832495
Mathieu Soucy <melkobelrose@gmail.com>
2021-08-27 14:08:27 -0400

    Cours 2

---
+++ S02/server.js
@@ -0,0 +1,21 @@
+import chalk from 'chalk';
+
+import app from './src/app.js';
+
+console.log(chalk.red('YO LES GEEEEEENS'));
+console.log(chalk.yellow('YO LES GEEEEEENS'));
+console.log(chalk.green('YO LES GEEEEEENS'));
+console.log(chalk.blue('YO LES GEEEEEENS'));
+console.log(chalk.magenta('YO LES GEEEEEENS'));
+console.log(chalk.inverse('YO LES GEEEEEENS'));
+console.log(chalk.bgRed('YO LES GEEEEEENS'));
+console.log(chalk.bgYellow('YO LES GEEEEEENS'));
+console.log(chalk.bgGreen('YO LES GEEEEEENS'));
+console.log(chalk.bgBlue('YO LES GEEEEEENS'));
+console.log(chalk.bgMagenta('YO LES GEEEEEENS'));
+
+const PORT = 5000;
+
+app.listen(PORT, err => {
+    console.log(chalk.bgRed(`Server listening on port: ${PORT}`));
+});
