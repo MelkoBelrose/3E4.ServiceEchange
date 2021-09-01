0a7300e16c1c4eb2785da23df1b236069bb90e25
Mathieu Soucy <melkobelrose@gmail.com>
2021-09-01 11:23:54 -0400

    cours 2 end

--- S02/src/app.js
+++ S02/src/app.js
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import express from 'express';
 
 const app = express();
@@ -8,17 +9,54 @@ app.get('/premiere', (req, res) =>{
     res.send('notre premiere route avec express');
 });
 
-app.get('/maths/somme', (req, res) =>{
-
-    //console.log(req.query);
-
+//produit, difference, quotient, reste, somme
+app.get('/maths/:operation', (req, res) =>{
+    
+    const operation = req.params.operation;
     const a = parseInt(req.query.a,10);
     const b = parseInt(req.query.b,10);
-    const somme = a + b;
+    let result;
+
+    switch(operation){
+        case 'somme':
+            result = a+b;
+            break;
+        case 'difference':
+            result = a-b;
+            break;
+        case 'produit':
+            result = a*b;
+            break;
+        case 'quotient':
+            result = a/b;
+            break;
+        case 'reste':
+            result = a%b;
+            break;
+        default:
+            console.log('What non')
+            return res.status(418).end();
+
+    }
 
     res.status(200);
     res.set('Content-Type', 'text/html');
-    res.send(`<b>${somme}</b>`);
+    res.send(`<b style="font-size:300px;color:red;background-color:black">${result}</b>`);
+
+});
+
+app.get('/date', (req, res) =>{
+
+    //console.log(req.query);
+
+    const d = parseInt(req.query.a,10);
+    const h = parseInt(req.query.b,10);
+
+    res.status(200).set('Content-Type', 'text/html');
+    
+    const now= dayjs().format('YYYY-MM-DD HH:mm:ss')
+
+    res.send(`<b style="font-size:90px">${now}</b>`);
 
 });
 
